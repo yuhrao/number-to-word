7fba428341e32641c0a4b5c6404172db5cd72665
Yuhri Graziano Bernardes <yuhri.graziano@gmail.com>
2022-02-21 15:28:17 -0300

    chore: add project url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject io.github.yuhribernardes/number-to-words "0.2.2"
   :description "Library to convert number to words"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/yuhribernardes/number-to-word"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.3"]]
