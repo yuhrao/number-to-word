2d61db147fbac8ee13817797088ffeacf4636acb
Yuhri Graziano Bernardes <yuhri.graziano@gmail.com>
2022-02-21 13:29:40 -0300

    ci: configure clojars deploy

--- project.clj
+++ project.clj
@@ -1,13 +1,16 @@
-(defproject number-to-words "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject io.github.yuhribernardes/number-to-words "0.2.2"
+  :description "Library to convert number to words"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.3"]]
-  :main ^:skip-aot number-to-words.core
+  :main number-to-words.parse
   :target-path "target/%s"
+  :aliases {"kaocha" ["with-profile" "+kaocha" "run" "-m" "kaocha.runner"]}
   :profiles {:uberjar {:aot :all
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}
              :kaocha {:dependencies [[lambdaisland/kaocha "1.62.993"]
                                      [nubank/matcher-combinators "3.3.1"]]}}
-  :aliases {"kaocha" ["with-profile" "+kaocha" "run" "-m" "kaocha.runner"]})
+  :repositories {"release" {:url "https://repo.clojars.org/"
+                            :username :env/clojars_user
+                            :password :env/clojars_pwd}})
